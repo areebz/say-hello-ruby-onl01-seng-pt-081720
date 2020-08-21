0aac89d3b5b29f9f72e146a75e8b59b48f41bd8a
Areeba Zahir <areebazahir3@gmail.com>
2020-08-21 22:39:51 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,7 +4,7 @@ say_hello("Gabriela")
 # The method prints this text to the screen:
 Hello Gabriela!
 
-# I call on the method, say_hello, and give it the string "Gabriela" 
+# I call on the method, say_hello, and give it the string "Hello Ruby Programmer" 
 say_hello("Hello Ruby Programmer!")
  
 # The method prints this text to the screen:
